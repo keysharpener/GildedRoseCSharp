af845e90f7ec54a4f2f776b738efb69887cfdac8
Robin Francillon <robin.francillon@gmail.com>
2016-05-18 01:02:01 +0200

    improved test coverage

--- GildedRose/GildedRoseTest.cs
+++ GildedRose/GildedRoseTest.cs
@@ -4,22 +4,28 @@ using NUnit.Framework;
 
 namespace GildedRose
 {
-	[TestFixture()]
-	public class GildedRoseTest
-	{
-		[TestCase("toto", 0, 1, 0)]
-	    public void Should_remove_quality(string itemName, int itemSellIn, int itemQuality, int expectedQuality)
-		{
-		    var itemToTest = new Item {Name = itemName, SellIn = itemSellIn, Quality = itemQuality};
+    [TestFixture()]
+    public class GildedRoseTest
+    {
+        [TestCase("toto", 0, 1, 0)]
+        [TestCase("Sulfuras, Hand of Ragnaros", 0, 1, 1)]
+        [TestCase("Aged Brie", 0, 1, 3)]
+        [TestCase("Backstage passes to a TAFKAL80ETC concert", 3, 5, 8)]
+        public void Should_correctly_handle_items_through_first_block(string itemName, int itemSellIn, int itemQuality,
+            int expectedQuality)
+        {
+            var itemToTest = new Item {Name = itemName, SellIn = itemSellIn, Quality = itemQuality};
 
             IList<Item> Items = new List<Item>
-			{
-			    itemToTest
-			};
+            {
+                itemToTest
+            };
             GildedRoseKata.GildedRose app = new GildedRoseKata.GildedRose(Items);
             app.UpdateQuality();
             Assert.AreEqual(expectedQuality, itemToTest.Quality);
         }
+
+      
     }
 }
 
